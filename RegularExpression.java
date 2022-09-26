9380d5b0f6678d364fcd489be8520e7280c2a9f7
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-26 22:43:31 +0530

    Add : Enter a valid last name by user

--- RegularExpression.java
+++ RegularExpression.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class RegularExpression {
-	//Checking for first name start with capital and has minimum 3 chararcters 
+	//Checking for first name start with capital and has minimum 3 characters 
 	public static boolean isValidFirstName(String firstName) {
         Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
 		Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
-
+	}
+	//Checking for last name start with capital and has minimum 3 characters 
+	public static boolean isValidLastName(String firstName) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
+		Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
 	}
 	public static void main(String[] args) {
 		//user input
 		Scanner name = new Scanner(System.in);
-		System.out.println("Enter your first Name starting with capital case :");
+		System.out.println("Enter your first name start with capital and has minimum 3 characters :");
 		String firstName=name.next();
+		System.out.println("Enter your last name start with capital and has minimum 3 characters :");
+		String lastName=name.next();
 		System.out.println(isValidFirstName(firstName));
+		System.out.println(isValidFirstName(lastName));
+
 }
 }
