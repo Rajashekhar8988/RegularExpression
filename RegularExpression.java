043ca48920e018795839250f7c3e562e0057c37a
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-26 23:36:19 +0530

    modified : to check the mobile format

--- RegularExpression.java
+++ RegularExpression.java
@@ -5,31 +5,46 @@ import java.util.regex.*;
 public class RegularExpression {
 	//Checking for first name start with capital and has minimum 3 characters 
 	public static boolean isValidFirstName(String firstName) {
-        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
 		Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
 	}
 	//Checking for last name start with capital and has minimum 3 characters 
-	public static boolean isValidLastName(String firstName) {
-        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}");
-		Matcher matcher = pattern.matcher(firstName);
+	public static boolean isValidLastName(String lastName) {
+        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+		Matcher matcher = pattern.matcher(lastName);
         return matcher.matches();
 	}
 	//method to check for a valid Email
 	public static boolean isValidEmail() {
-	Pattern pattern = Pattern.compile("^[a][b][c].[x][y][z]+@[b][l].[c][o].[i][n]");
+	Pattern pattern = Pattern.compile("^[a][b][c].[x][y][z]+@[b][l].[c][o].[i][n]$");
 	Matcher matcher = pattern.matcher("abc.xyz@bl.co.in");
     return matcher.matches();
-    
-	}	public static void main(String[] args) {
+	}	
+	//	//method to check for a valid mobile number
+	public static boolean isValidMobileNumber() {
+		Pattern pattern = Pattern.compile("[+][1-9]{1,3} [0-9]{10}$");
+		Matcher matcher = pattern.matcher("+91 9876543212");
+	    return matcher.matches();
+	    
+		}	
+	public static void main(String[] args) {
 		//user input
 		Scanner name = new Scanner(System.in);
 		System.out.println("Enter your first name starts with capital and has minimum 3 characters :");
 		String firstName=name.next();
+		System.out.println(isValidFirstName(firstName));
 		System.out.println("Enter your last name starts with capital and has minimum 3 characters :");
 		String lastName=name.next();
-		System.out.println(isValidFirstName(firstName));
 		System.out.println(isValidFirstName(lastName));
 		System.out.println(isValidEmail());
+		System.out.println("Enter your mobile number with Country code follow by space and 10 digit number");
+		String mobileNumber=name.next();
+		if(isValidMobileNumber()){
+			System.out.println("Given Number is a Valid Number");
+		}
+		else {
+			System.out.println("OOPS...Please enter a valid Number");
+		}
 }
 }
