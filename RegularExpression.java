67d1ad4e40e5d4f30d96e3fad936e707e666dda4
Janga Rajashekhar Reddy <rajashekharraj3@gmail.com>
2022-09-26 22:59:49 +0530

    Add : Enter a Email by the user

--- RegularExpression.java
+++ RegularExpression.java
@@ -15,15 +15,21 @@ public class RegularExpression {
 		Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
 	}
-	public static void main(String[] args) {
+	//method to check for a valid Email
+	public static boolean isValidEmail() {
+	Pattern pattern = Pattern.compile("^[a][b][c].[x][y][z]+@[b][l].[c][o].[i][n]");
+	Matcher matcher = pattern.matcher("abc.xyz@bl.co.in");
+    return matcher.matches();
+    
+	}	public static void main(String[] args) {
 		//user input
 		Scanner name = new Scanner(System.in);
-		System.out.println("Enter your first name start with capital and has minimum 3 characters :");
+		System.out.println("Enter your first name starts with capital and has minimum 3 characters :");
 		String firstName=name.next();
-		System.out.println("Enter your last name start with capital and has minimum 3 characters :");
+		System.out.println("Enter your last name starts with capital and has minimum 3 characters :");
 		String lastName=name.next();
 		System.out.println(isValidFirstName(firstName));
 		System.out.println(isValidFirstName(lastName));
-
+		System.out.println(isValidEmail());
 }
 }
